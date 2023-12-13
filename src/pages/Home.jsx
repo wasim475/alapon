7083379e1aa9145e1bcda1223d7e8d9a01eb7c14
wasim475 <wasimhossain575@gmail.com>
2023-12-13 08:35:59 +0600

    sign out done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = () => {
+  let navigate = useNavigate()
+  const auth = getAuth();
+
+  let handleLogout = ()=>{
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+    navigate('/login')
+  }
   return (
-    <div>Home</div>
+    <>
+      <Button onClick={handleLogout} variant='contained'>Log Out</Button>
+    </>
   )
 }
 
